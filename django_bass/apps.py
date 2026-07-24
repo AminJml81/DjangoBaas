c56df7277dfe502596c9d99f8b10b3d9f107d8c3
Amin <Aminbest1381@gmail.com>
2026-07-24 23:13:51 +0330

    feat: initial commit

---
+++ django_bass/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoBaasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_baas'
+    verbose_name = 'Django Baas Engine'
